84203f44fbbbcf74d0a3615d666ba3ee61ad5c8c
famille.bertrand.lauriane <famille.bertrand.lauriane@gmail.com>
2021-04-07 11:22:37 +0200

    JSON file for tasks

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,16 +1,36 @@
 package com.example.myapplication;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private File taskFile;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,5 +52,54 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        initializeJSONTasks();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void initializeJSONTasks() {
+        taskFile = new File(this.getFilesDir(), "tasks.json");
+        Log.d("path", taskFile.getAbsolutePath());
+
+        FileWriter fileWriter = null;
+
+        if (!taskFile.exists()) {
+            Log.d("taskFile", "created");
+            try {
+                taskFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            fileWriter = new FileWriter(taskFile.getAbsoluteFile());
+            Log.d("path 2", taskFile.getAbsolutePath());
+
+            // add premade tasks
+            Task newTask1 = new Task("Exam", "My exam", "IGR203", "06-07-2021",
+                    "12:13", "My description", 3, 1, new String[]{"exam", "igr203"}, true);
+            Task newTask2 = new Task("Meeting", "My meeting", "IGR203", "20-04-2021",
+                    "16:00", "My description", 1, 4, new String[]{"meeting", "igr203"}, true);
+            Task newTask3 = new Task("Project", "My project", "IGR203", "03-05-2021",
+                    "23:59", "My description", 4, 4, new String[]{"project", "igr203"}, true);
+
+            List<Task> tasks = Arrays.asList(newTask1, newTask2, newTask3);
+
+            Writer writer = Files.newBufferedWriter(Paths.get(taskFile.getPath()));
+
+            // create Gson instance
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+            // convert user object to JSON file
+            gson.toJson(tasks, writer);
+
+            // close writer
+            writer.close();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
