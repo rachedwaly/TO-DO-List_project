c934c7750c2f998f302823dc750e4bc52139bbbd
Rached <rachedwaly1998@gmail.com>
2021-04-08 00:32:33 +0200

    popup fragment to show details about a task without any design

--- app/src/main/java/com/example/myapplication/AdapterForCards.java
+++ app/src/main/java/com/example/myapplication/AdapterForCards.java
@@ -1,11 +1,15 @@
 package com.example.myapplication;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
+import android.os.Build;
 import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.view.MotionEventCompat;
@@ -15,23 +19,35 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 
+
 public class AdapterForCards extends RecyclerView.Adapter<AdapterForCards.ViewHolder>  {
     private ArrayList<Card> list;
+    private OnCardListener mOnCardListener;
 
-    public AdapterForCards(ArrayList<Card> cardList) {
+
+    public AdapterForCards(ArrayList<Card> cardList, OnCardListener onCardListener) {
         list=cardList;
+        this.mOnCardListener=onCardListener;
     }
 
 
 
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         public TextView mTextView1;
         public TextView mTextView2;
-
-        public ViewHolder(@NonNull View itemView) {
+        OnCardListener onCardListener;
+        public ViewHolder(@NonNull View itemView, OnCardListener onCardListener) {
             super(itemView);
             mTextView1 = itemView.findViewById(R.id.textView);
             mTextView2 = itemView.findViewById(R.id.textView2);
+            this.onCardListener=onCardListener;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            onCardListener.onCardClick(getAdapterPosition());
+
         }
     }
 
@@ -41,10 +57,9 @@ public class AdapterForCards extends RecyclerView.Adapter<AdapterForCards.ViewHo
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.card_item,parent,false);
-        ViewHolder holder=new ViewHolder(v);
 
 
-        return holder;
+        return new ViewHolder(v,mOnCardListener);
     }
 
 
@@ -55,10 +70,17 @@ public class AdapterForCards extends RecyclerView.Adapter<AdapterForCards.ViewHo
         holder.mTextView2.setText(currentItem.getText2());
 
 
+
     }
 
     @Override
     public int getItemCount() {
         return list.size();
     }
+
+    public interface OnCardListener{
+        void onCardClick(int position);
+    }
+
+
 }
--- app/src/main/java/com/example/myapplication/Main.java
+++ app/src/main/java/com/example/myapplication/Main.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.content.ClipData;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Rect;
 import android.os.Bundle;
 
@@ -12,9 +14,15 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -23,6 +31,10 @@ import java.util.ArrayList;
  * Use the {@link Main#newInstance} factory method to
  * create an instance of this fragment.
  */
+
+
+
+
 
 //class for spacing the cards withing the gridlayout
 class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
@@ -71,11 +83,14 @@ class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
 }
 
 
-public class Main extends Fragment {
+public class Main extends Fragment implements AdapterForCards.OnCardListener {
     private ArrayList<Card> cardList = new ArrayList<>();
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private FloatingActionButton mRecycleBinButton;
+    private Card cardToDelete;
+    private int newContactPosition = -1;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -115,12 +130,16 @@ public class Main extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
 
-        cardList.add(new Card("Line 1hjgjgjgjgjjjgjgj", "Line 2jhfhjfjgjgjgjgjgjgj"));
+        cardList.add(new Card("Line", "Line 2jhfhjfjgjgjgjgjgjgj"));
         cardList.add(new Card("Line 3", "Line 4"));
         cardList.add(new Card("Line 5", "Line 6"));
         cardList.add(new Card("Line 5", "Line 6"));
         cardList.add(new Card("Line 5", "Line 6"));
-
+        cardList.add(new Card("Line 5", "Line 6"));
+        cardList.add(new Card("Line 5", "Line 6"));
+        cardList.add(new Card("Line 5", "Line 6"));
+        cardList.add(new Card("Line 3", "Line 4"));
+        cardList.add(new Card("Line 3", "Line 4"));
 
 
 
@@ -129,20 +148,19 @@ public class Main extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 
-
+        mRecycleBinButton=getView().findViewById(R.id.recycleFab);
         mRecyclerView = getView().findViewById(R.id.recyclerView);
-        //mRecyclerView.setHasFixedSize(true);
-        //mLayoutManager = new LinearLayoutManager(getActivity());
+
         mLayoutManager = new GridLayoutManager(getActivity(),2);
 
 
-        mAdapter = new AdapterForCards(cardList);
+        mAdapter = new AdapterForCards(cardList,this);
 
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
-        mRecyclerView.addItemDecoration(new GridSpacingItemDecoration(2, 25, true, 0));
+        mRecyclerView.addItemDecoration(new GridSpacingItemDecoration(2, 40, true, 0));
 
-        ItemTouchHelper itemTouchHelper=new ItemTouchHelper(simpleCallback);
+        ItemTouchHelper itemTouchHelper=new ItemTouchHelper(Callback);
         itemTouchHelper.attachToRecyclerView(mRecyclerView);
 
     }
@@ -154,19 +172,76 @@ public class Main extends Fragment {
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
-    ItemTouchHelper.SimpleCallback simpleCallback= new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP|ItemTouchHelper.DOWN|
-            ItemTouchHelper.RIGHT|ItemTouchHelper.LEFT,0) {
+    ItemTouchHelper.Callback Callback= new ItemTouchHelper.Callback() {
+
+        @Override
+        public int getMovementFlags(RecyclerView recyclerView,
+                                    RecyclerView.ViewHolder viewHolder) {
+            int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN|ItemTouchHelper.RIGHT|ItemTouchHelper.LEFT;
+            int swipeFlags = 0; //no swipe needed
+            return makeMovementFlags(dragFlags, swipeFlags);
+        }
+
         @Override
         public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
-            viewHolder.itemView.setBackgroundColor(1);
+
+            moveItem(viewHolder.getAdapterPosition(),target.getAdapterPosition());
+            if (isViewOverlapping(viewHolder.itemView, mRecycleBinButton)) {
+                Toast.makeText(getActivity(),"card deleted",Toast.LENGTH_SHORT).show();
+                cardList.remove(viewHolder.getAdapterPosition());
+
+                mAdapter.notifyDataSetChanged();
+            }
             return false;
         }
 
+
+
+
+
+
         @Override
         public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
-
         }
+
+
+
+
+
     };
+
+    //rearrange cards when dragging cards
+    private void moveItem(int oldPos, int newPos){
+        Card item=(Card) cardList.get(oldPos);
+        cardList.remove(oldPos);
+        cardList.add(newPos,item);
+        mAdapter.notifyItemMoved(oldPos,newPos);
+    }
+
+    //check if the card is on the bin icon
+    private boolean isViewOverlapping(View firstView, View secondView) {
+
+        int[] secondPosition = new int[2];
+
+        int[] firstPosition = new int[2];
+        firstView.getLocationOnScreen(firstPosition);
+
+        secondView.getLocationOnScreen(secondPosition);
+
+        return firstPosition[0] < secondPosition[0] + secondView.getMeasuredWidth()
+                && firstPosition[0] + firstView.getMeasuredWidth() > secondPosition[0]
+                && firstPosition[1] < secondPosition[1] + secondView.getMeasuredHeight()
+                && firstPosition[1] + firstView.getMeasuredHeight() > secondPosition[1];
+    }
+
+    @Override
+    public void onCardClick(int position) {
+        //position is the item index in the list of cards
+        CardDetailedFragment cardDetailedFragment=new CardDetailedFragment();
+        cardDetailedFragment.show(getFragmentManager(),"TaskDetailed");
+
+    }
+
 
 }
 
